f8ef10615ab17385911350888c8d8ba5782884eb
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-01-22 14:12:54 +0100

    changed readme

--- src/ex5.ts
+++ src/ex5.ts
@@ -1,34 +1,30 @@
-
-
-
-    // Skapa en typ "Person" som ska vara en union type  av "User", "Admin" och "SuperAdmin"
-
+// Skapa en typ "Person" som ska vara en union type  av "User", "Admin" och "SuperAdmin"
 
 const user: any = "Admin";
-    
+
 const getPriviligies = (user: any): any => {
 
-  switch (user) {
+    switch (user) {
 
-    case "User":
+        case "User":
 
-      console.log("You have user right privileges");
+            console.log("You have user right privileges");
 
-      break;
+            break;
 
-    case "Admin":
+        case "Admin":
 
-      console.log("You have admin right privileges");
+            console.log("You have admin right privileges");
 
-      break;
+            break;
 
-    case "SuperAdmin":
+        case "SuperAdmin":
 
-      console.log("You have super admin right privileges");
+            console.log("You have super admin right privileges");
 
-      break;
+            break;
 
-  }
+    }
 
 };
 
